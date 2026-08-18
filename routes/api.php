106ce714524463b7f66e00edef2eeaa0d1bbfe43
刘骏霖 <763606865@qq.com>
2026-08-18 11:09:36 +0800

    feat： 1. 系统设置Api接口

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Api\Controllers\AuthController;
 use App\Api\Controllers\ClientVersionController;
 use App\Api\Controllers\DeviceController;
 use App\Api\Controllers\FileController;
+use App\Api\Controllers\SystemSettingController;
 use App\Api\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,7 @@ Route::prefix('auth')->group(function (): void {
 
 Route::post('/devices/sync', [DeviceController::class, 'sync'])->middleware('throttle:10,1');
 Route::get('/client/version/check', [ClientVersionController::class, 'check'])->middleware('throttle:60,1');
+Route::get('/system/settings', SystemSettingController::class)->middleware('throttle:60,1');
 
 Route::middleware('auth:api')->group(function (): void {
     // 文件上传
